315ae5df1f97878bb9e4d0d3a927ae05eea53bce
Karl Lorey <git@karllorey.com>
2019-04-05 22:01:22 +0200

    Initial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A project structure for data-intensive, latex-based reports',
+    author='Karl Lorey',
+    license='MIT',
+)
